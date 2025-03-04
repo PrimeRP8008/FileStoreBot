556a537b29c5e0256b54f61480721debfbca97cb
PrimeRP8008 <rohitpanghal3031@gmail.com>
2025-03-04 20:35:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from os import getenv
 
 
-API_ID = int(getenv("API_ID", "26850449"))
-API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
+API_ID = int(getenv("API_ID", "27419615"))
+API_HASH = getenv("API_HASH", "2f4b181296f0a2615a85471a1c72df44")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", "6107581019"))
-MONGO_DB = getenv("MONGO_DB", "")
+OWNER_ID = int(getenv("OWNER_ID", "1534632634"))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://PrimeRp:JNTymVXjKzBbDtiv@cluster0.rydwk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
